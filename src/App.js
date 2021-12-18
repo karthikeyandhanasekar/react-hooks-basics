f08191cd08553217bd2015bd03b6ba30f296d2b4
Karthik <dkkarthik2000@gmail.com>
2021-12-18 10:13:23 +0530

    react init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  const [state, setstate] = React.useState()
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
